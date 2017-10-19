b0f6ba8db90d09d10655a343df25d49b83755a51
y <yasu.ellegarden@gmail.com>
2017-10-19 10:16:15 +0900

    realmCRUDのUD全然対応できていない

---
+++ sampleBlog/BlogListCell.swift
@@ -0,0 +1,24 @@
+//
+//  BlogListCell.swift
+//  sampleBlog
+//
+//  Created by y on 2017/10/18.
+//  Copyright © 2017年 onono. All rights reserved.
+//
+
+import UIKit
+
+class BlogListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ sampleBlog/BlogRealm.swift
@@ -0,0 +1,13 @@
+//
+//  BlogRealm.swift
+//  sampleBlog
+//
+//  Created by y on 2017/10/18.
+//  Copyright © 2017年 onono. All rights reserved.
+//
+
+import UIKit
+
+class BlogRealm: Object {
+
+}
